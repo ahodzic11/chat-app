6c4c50b17c5d0bb48b3fb3afa4dabaa2dc4b6f2d
ahodzic11 <ahodzic11@etf.unsa.ba>
2023-02-17 17:11:17 +0100

    Added logo and basic information on the home page.

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import { Nav, Navbar, Container, Button, NavDropdown  } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import logo from '../assets/logo.png'
 
 function Navigation() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <LinkContainer to="/">
+             <Navbar.Brand>
+                <img id='logo' src={logo} alt="ChatAppLogo"/>
+             </Navbar.Brand>
+        </LinkContainer>
+        
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="ms-auto">
+
+            <LinkContainer to="/">
+                <Nav.Link >Home</Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to="/chat">
+                <Nav.Link >Chat</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
