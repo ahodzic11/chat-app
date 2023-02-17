3312ccc0acccd915213e2f92cfb4dae50b707de6
ahodzic11 <ahodzic11@etf.unsa.ba>
2023-02-17 18:11:18 +0100

    Name validation added.

--- frontend/src/pages/TemporaryLogin.js
+++ frontend/src/pages/TemporaryLogin.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, Container, Row, Col } from "react-bootstrap";
 import "./TemporaryLogin.css";
 
 function TemporaryLogin() {
+  const [name, setName] = useState("");
+
+  function handleLogin(e) {
+    e.preventDefault();
+    if (name.length < 3 || name.length > 15) return alert("Name must be between 3 and 15 characters!");
+  }
+
   return (
     <Container>
       <Row>
         <Col md={5} className="login__bg"></Col>
         <Col md={7} className="d-flex align-items-center justify-content-center flex-direction-column">
-          <Form style={{ width: "80%", maxWidth: 500 }}>
+          <Form style={{ width: "80%", maxWidth: 500 }} onSubmit={handleLogin}>
             <Form.Group className="mb-3">
               <Form.Label>Username</Form.Label>
-              <Form.Control placeholder="Enter username" />
+              <Form.Control placeholder="Enter username" onChange={(e) => setName(e.target.value)} value={name} required />
               <Form.Text className="text-muted">Random name will be generated if desired username is taken.</Form.Text>
             </Form.Group>
 
